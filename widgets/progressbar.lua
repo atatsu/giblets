74bad521566bd58865b59a2305b94e2be445bd97
atatsu <nathan.lundquist@gmail.com>
2015-01-07 20:00:46 -0600

    progressbar  * just added a few function placeholders

--- widgets/progressbar.lua
+++ widgets/progressbar.lua
@@ -3,12 +3,18 @@
 -- @author Nathan Lundquist (atatsu)
 -- @copyright 2015 Nathan Lundquist
 
+local base = require("wibox.widget.base")
+
 local Progressbar = {}
 Progressbar.__index = Progressbar
 
 function Progressbar.new()
   local self = setmetatable({}, Progressbar)
+  self.widget = base.make_widget()
   return self
+end
+
+function Progressbar:draw(wibox, cr, width, height)
 end
 
 return setmetatable(Progressbar, {
